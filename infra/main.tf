df20cc6f7a584bfdbf9e388945d6004f69b7d450
karthikeyan.b2 <karthikeyan.b@circles.asia>
2024-06-16 12:28:40 +0800

    Create Azure Devops project and Service connections using TF

---
+++ infra/main.tf
@@ -0,0 +1,44 @@
+
+
+resource "azuredevops_project" "project" {
+  name               = var.project_name
+  visibility         = "private"
+  version_control    = "Git"
+  work_item_template = "Agile"
+  description        = "Managed by Terraform"
+}
+
+resource "azuredevops_serviceendpoint_github" "github" {
+  project_id            = azuredevops_project.project.id
+  service_endpoint_name = "GitHub Service Connection"
+  auth_personal {
+    personal_access_token = var.github_pat
+  }
+}
+
+resource "azuredevops_serviceendpoint_azurerm" "azurerm" {
+  project_id                             = azuredevops_project.project.id
+  service_endpoint_name                  = "Azure Service Connection"
+  service_endpoint_authentication_scheme = "ServicePrincipal"
+  description                            = "Managed by Terraform"
+  credentials {
+    serviceprincipalid  = var.client_id
+    serviceprincipalkey = var.client_secret
+  }
+
+  azurerm_spn_tenantid      = var.tenant_id
+  azurerm_subscription_id   = var.subscription_id
+  azurerm_subscription_name = var.subscription_name
+}
+
+resource "azuredevops_build_definition" "pipeline" {
+  project_id = azuredevops_project.project.id
+  name       = "WebGoat DevSecOps Pipeline"
+  path       = "\\"
+  repository {
+    repo_type   = "GitHub"
+    repo_id     = var.github_repository_name
+    branch_name = "main"
+    yml_path    = "azure-pipelines.yml"
+  }
+}
